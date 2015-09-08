05ccf0f0abdc2d11aa42fd9e024a8747043378e9
sK0pe <pradyumn.vij@gmail.com>
2015-09-09 01:44:08 +0800

    mainArgCheck

--- whichbus.c
+++ whichbus.c
@@ -53,26 +53,95 @@ int   test  = 0;
 
 int   flag = 0;
 
-
-bool valid_location(double latitude, double longitude)
-{
-	return (latitude  >= -90.0  && latitude  <= 90.0  &&
-			longitude >= -180.0 && longitude <= 180.0);
+/*
+ *	valid_location
+ *	boolean
+ *	Sanity check function for coordinates
+ *	returns true iff coordinates are correct
+ *	else returns false
+ */
+static bool valid_location(double latitude, double longitude){
+	return ((latitude  >= -90.0  && latitude  <= 90.0) &&
+			(longitude >= -180.0 && longitude <= 180.0));
 }
 
-double haversine(double lat1, double lon1, double lat2, double lon2) 
-{
-	double deltalat = (lat2 - lat1) / 2.0;
-	double deltalon = (lon2 - lon1) / 2.0;
-	double sin1     =	sin(deltalat * M_PI / 180.0);
-	double cos1     = cos(lat1     * M_PI / 180.0);
-	double cos2     = cos(lat2     * M_PI / 180.0);
-	double sin2     = sin(deltalon * M_PI / 180.0);
-	double x        = sin1*sin1 + cos1*cos2 * sin2*sin2;
-	double distance = 2 * EARTH_RADIUS_M * asin(sqrt(x));
-
-	return distance;
+/*
+ *	degrees_to_radians
+ *	double
+ * 	Helper function for haversine function
+ * 	Converts degrees to radians in type
+ *
+ */
+static double degrees_to_radians(double degrees){
+	return degrees*PI/180;
 }
+
+
+/*
+ *	haversine
+ *	double
+ *	Uses Haversine formula,
+ *	see https://en.wikipedia.org/wiki/Haversine_formula
+ *	Calculates the distance in metres between
+ *	2 coordinates
+ */
+static double haversine(double lat1, double lon1, double lat2, double lon2){
+	lat1 = degrees_to_radians(lat1);
+	lon1 = degrees_to_radians(lon1);
+	lat2 = degrees_to_radians(lat2);
+	lon2 = degrees_to_radians(lat2);
+	
+	return (2.0 * EARTH_RADIUS_M * 
+			asin(
+					sqrt(
+						pow((sin((lat2 - lat1)/2.0)),2) 
+							+ cos(lat1) * cos(lat2) * pow(sin((lon2 - lon1)/2.0),2))));
+}
+
+/*
+ *	walk
+ *	void
+ *	Print function for walking
+ *
+ */
+static void walk(int minutes, int distance, char[] destination){
+	printf("%i:%i walk %im to %s", time/60, time%60, distance, destination);
+}
+
+
+/*
+ *	origin_to_stop
+ *	Determine distance to destination and finds optimal public transport
+ *	stop to walk to (if available).
+ *	
+ */
+static void origin_to_stop(double lat1, double lon1, double lat2, double lon2){
+	double distance = haversine(lat1, lon1, lat2, lon2);
+	if(distance < MAX_WALKING_DIST){
+		stop_to_destination(0, distance);
+	}
+}
+
+/*
+ *	stop_to_stop
+ */
+
+/*
+ * 	stop_to_destination
+ */
+void stop_to_destination(int minutes, double final_Walk){
+	int distance = (int) ceil(final_Walk);
+	//	print walk line
+	walk(minutes, distance, "destination");
+	//	print arrive line
+	minutes += distance;
+	printf("%i:%i arrive", minutes/60, minutes%60);
+}
+
+/*
+ */
+
+
 
 // splits line using delimeter
 void split(char line[], char del, char splitter[][N_FIELDS][N_COLS]) 
@@ -315,33 +384,38 @@ void process_stop()
 	}
 } // stops.txt
 
-int main(int argc, char **argv)
-{
-	int result;
 
-	if (argc != 6) 
-	{ 
-		printf("%% Usage: ./whichbus <directory> <Source coordinates> <Destn coordinates>\n");
-		result = EXIT_FAILURE;
+/*
+ *	Main Function
+ *	Deals with input argument validity and passing variables as required
+ */
+int main(int argc, char *argv[]){
+
+	if (argc != 6){
+		fprintf(stderr,"%s Usage: <directory>\n <Origin Latitude> <Origin Longitude>\n \
+											<Destination Latitude> <Destination Longitude>\n Coordinates \
+											must be entered as positive or negative numbers.\n",argv[0]);
+		exit(EXIT_FAILURE);
 	}
-	else
-	{
-		lat1 = atof(argv[2]);
-		lon1 = atof(argv[3]);
-		lat2 = atof(argv[4]);
-		lon2 = atof(argv[5]);
-		if (valid_location(lat1, lon1) == false)
-		{
-			printf("%s: Location 1 is invalid!\n", argv[0]);
-			result = EXIT_FAILURE;
+	else{
+		origin_Lat = atof(argv[2]);
+		origin_Lon = atof(argv[3]);
+		dest_Lat = atof(argv[4]);
+		dest_Lon = atof(argv[5]);
+		//	Check if Origin coordinates correct
+		if (!valid_location(origin_Lat,origin_Lon)){
+			fprintf(stderr,"%s Error: Origin Coordinates invalid!\n", argv[0]);
+			exit(EXIT_FAILURE);
 		}
-		else if (valid_location(lat2, lon2) == false)
-		{
-			printf("%s: Location 2 is invalid!\n", argv[0]);
-			result = EXIT_FAILURE;
-		}
-		else
-		{
+		//	Check if Destination coordinates correct
+		if (!valid_location(dest_Lat, dest_Lon)){
+			fprintf(stderr,"%s Error: Destination Coordinates invalid!\n", argv[0]);
+			exit(EXIT_FAILURE);
+		}c double to int
+		//	
+		origin_to_stop(origin_Lat, origin_Lon, dest_Lat, dest_Lon);
+
+		else{
 			finalDistance = haversine(lat1, lon1, lat2, lon2); // Total Travel distance...
 			tempDistance  = finalDistance;
 
@@ -382,8 +456,8 @@ int main(int argc, char **argv)
 				process_stop();
 			}
 			closeFiles();
-			result = EXIT_SUCCESS;
+			exit(EXIT_SUCCESS);
 		}
 	}
-	return result;
+	return 0;
 }
