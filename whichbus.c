6aa1024871ec120a3596d55db54a199be8cea5f8
sK0pe <pradyumn.vij@gmail.com>
2015-09-13 17:37:11 +0800

    Functional delimiter / Tokenizer working

--- whichbus.c
+++ whichbus.c
@@ -3,10 +3,41 @@
 #include <stdbool.h>
 #include <math.h>
 #include <time.h>
+#include <string.h>
 
 #define	MAX_WALKING_DIST 1000
 #define EARTH_RADIUS_M 6372797
 #define PI (acos(-1.0))
+//	Filenames
+#define ROUTES		"/routes.txt"
+#define STOPS			"/stops.txt"
+#define TRIPS			"/trips.txt"
+#define STOPTIMES	"/stop_times.txt"
+//	Global Variables
+int DAY;
+
+//	Struct Definitions
+
+/*	
+ *	Route struct
+ *	Holds pertinent data for Route information
+ */
+typedef struct{
+	int id;
+	char[4] routeNumber;
+	char[50] routeName;
+	int type;
+} Route;
+
+/*
+ *	
+ */
+typedef struct{
+	int id;
+} Trip;
+
+
+
 
 /*
  *	valid_location
@@ -42,39 +73,117 @@ static double degrees_to_radians(double degrees){
  *	2 coordinates
  */
 static double haversine(double lat1, double lon1, double lat2, double lon2){
+	double deltaLat = sin(degrees_to_radians(lat2 - lat1))/2.0;
+	double deltaLon = sin(degrees_to_radians(lon2 - lon1))/2.0;
 	lat1 = degrees_to_radians(lat1);
-	lon1 = degrees_to_radians(lon1);
 	lat2 = degrees_to_radians(lat2);
-	lon2 = degrees_to_radians(lat2);
 	
 	return (2.0 * EARTH_RADIUS_M * 
 			asin(
 					sqrt(
-						pow((sin((lat2 - lat1)/2.0)),2) 
-							+ cos(lat1) * cos(lat2) * pow(sin((lon2 - lon1)/2.0),2))));
+						pow(deltaLat,2) + cos(lat1) * cos(lat2) * pow(deltaLon,2))));
 }
+
 
 /*
  * getTime
  * int
  * returns int representing minute of day based on system time
+ * and assigns the global variable DAY an integer
  */
 int getTime(){
-	char hour[3], min[3];
+	char hour[3], min[3], day[4];
 	time_t currentTime;
 	//	call current time in seconds from local system
 	time(&currentTime);
 	//	format to a tm struct details for local system
 	struct tm *detail = localtime(&currentTime);
-	//	extract hour and minute in string form
+	//	extract hour, minute and day in string form
 	//	could also have extracted date and day 
 	strftime(hour, 3, "%H", detail);
 	strftime(min, 3, "%M", detail);
-	//	convert to int 
-	int h = atoi(hour);
-	int m = atoi(min);
+	strftime(day, 4, "%a", detail);
+	//	assign global DAY the appropiate day
+	if(strcmp(day,"Mon")==0) DAY = 1;
+	else if(strcmp(day,"Tue")==0) DAY = 2;
+	else if(strcmp(day,"Wed")==0) DAY = 3;
+	else if(strcmp(day,"Thu")==0) DAY = 4;
+	else if(strcmp(day,"Fri")==0) DAY = 5;
+	else if(strcmp(day,"Sat")==0) DAY = 6;
+	else if(strcmp(day,"Sun")==0) DAY = 7;
+	else printf("getTime : Invalid day sourced from System");
 	
+
+	//	convert time to int 
+	int h = atoi(hour);
+	int m = atoi(min);	
 	return h*60 + m;
+}
+
+/*
+ *	tokenizer
+ *	*char
+ *	Takes a char array pointer as the source string,
+ *	and a string indicating the delimiter on which to
+ *	split the string.
+ *	Outputs each successive token from string including
+ *	empty positions.
+ */
+char *tokenizer(char *source, const char *delimiter){
+	static char *cursor = NULL;
+	char *tokenStart;
+	int n;
+	//	First time, point to start of source
+	if(source != NULL){
+		cursor = source;
+	}
+	//	check if end of line
+	if(cursor == NULL || *cursor == '\0'){
+		return NULL;
+	}
+
+	// number of characters up to the next delimiter
+	// from cursor's position
+	n = strcspn(cursor, delimiter);
+	// save cursor pointer
+	tokenStart = cursor;
+	// move cursor forward the token length
+	cursor += n;
+	// if cursor is not pointing to a nullbyte
+	// overwrite the delimiter with nullbyte and move forward
+	if(*cursor != '\0'){
+		*cursor++ = '\0';
+	}
+	// return from cursor's position to new nullbyte
+	return tokenStart;
+}
+ 
+/*
+ *	getRouteName
+ *	char*
+ *	Takes integer representation of route name as input
+ *	returns char* with route name,
+ *	if no route name returns "bus"
+ */
+char *getRouteName(int route){
+	FILE *routeData = fopen(ROUTES, "r");
+	char line[BUFSIZ];
+	char routeName[50];
+	char id[4];
+	bool first = true;
+
+	while(fgets(line, sizeof line, routeData) != NULL){
+		if(first){
+			first = false;
+			continue;
+		}
+		sscanf( line, "%[^','],%*[^','],%*[^','],%[^','],%*[^','],%*[^','],%*[^','],%*[^','],%*s", id, routeNumber,routeName);
+		if(route == atoi(id)){
+			break;
+		}
+	}
+	if(strcmp(routeName, "")==0) return "bus";
+	return routeName;
 }
 
 /*
